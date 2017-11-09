56e99d099b0142bfeaca514c7693e04336f1c1f6
Natalia Balabonkina <nataliabalabonkina@gmail.com>
2017-11-09 12:26:22 +0200

    first com

---
+++ 1.js
@@ -0,0 +1,14 @@
+function pow(){
+    var a;
+    var b;
+    a=document.getElementById('inp1').value;
+    b=document.getElementById('inp2').value;
+    a=parseInt(a);
+    b=parseInt(b);
+    if(a>b){
+        alert(a);
+    }
+    else{
+        alert(b);
+    }
+}
